54bf1255bc2607950b8d4c676cfb261a3122df70
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-09-27 19:48:55 +0200

    changes2

--- Znaki.js
+++ Znaki.js
@@ -3,6 +3,6 @@ var dinosaur = 'triceratops'
 
 var dino = dinosaur.toUpperCase()
 
-var new = text.replace(Velociraptor', dinosaur);
+var new = text.replace(Velociraptor', 'Triceratops')
 
 console.log(text.length/2)
